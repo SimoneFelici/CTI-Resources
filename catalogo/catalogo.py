7317a2a041fdb69d9f4f94f2caafc3309fd4cf79
SimoneFelici <qualcosa588@gmail.com>
2024-08-29 14:10:53 +0200

    b

---
+++ catalogo/catalogo.py
@@ -0,0 +1,80 @@
+from requests_html import HTMLSession
+from datetime import datetime
+from googletrans import Translator
+from urllib.parse import urljoin
+import xlsxwriter
+from keywords import keywords
+
+def get_quarter(month):
+    return (month - 1) // 3 + 1
+
+def get_month_in_quarter(month):
+    return (month - 1) % 3 + 1
+
+session = HTMLSession()
+translator = Translator()
+i = 14
+data = []
+current_date = datetime.now()
+processed_links = set()
+
+while i > 0:
+    r = session.get(f'https://catalogodefraudes.rnp.br/?page={i}')
+    print(i)
+    links = []
+    base_url = 'https://catalogodefraudes.rnp.br'
+    for a_tag in r.html.find('a'):
+        href = a_tag.attrs.get('href')
+        if href and "frauds" in href:
+            full_link = urljoin(base_url, href)
+            if full_link not in processed_links:
+                links.append(full_link)
+                processed_links.add(full_link)
+    for link in reversed(links):
+        print(link)
+        r = session.get(link)
+        raw_date = r.html.xpath('//*[@id="text-page"]/h4[2]/text()', first=True)
+        text = r.html.xpath('//*[@id="text-page"]/p/text()', first=True)
+        date = raw_date.split(':')[1].strip()
+        date = datetime.strptime(date, '%d/%m/%y')
+        year = date.year
+        month = date.month
+        quarter = get_quarter(month)
+        month_in_quarter = get_month_in_quarter(month)
+        translated_text = translator.translate(text, src='pt', dest='it').text
+        
+        # Valori di default
+        severity = "medium"
+        sector = ""
+        damage = ""
+        tag = ""
+        
+        for keyword, changes in keywords.items():
+            if keyword.lower() in translated_text.lower():
+                severity = changes.get("severity", severity)
+                sector = changes.get("sector", sector)
+                tag = changes.get("tag", tag)
+                damage = changes.get("damage", damage)
+                break
+        
+        # Aggiungi una riga alla lista dei dati
+        data.append(['many', 'all', '', 'Brazil', 'attacco', 'cybercrime', damage, month_in_quarter, quarter, year, sector, 'Phishing/Social Engineering', 
+                     severity, '', '', '', tag, translated_text, '', '', 'Simone Felici', link, current_date, 'unknown', 
+                     'unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'unknown', 
+                     'unknown', 'unknown', 'unknown', 'unknown', 'unknown', 'NO', '' ])
+    
+    i -= 1
+
+workbook = xlsxwriter.Workbook('output.xlsx')
+worksheet = workbook.add_worksheet()
+
+date_format = workbook.add_format({'num_format': 'dd/mm/yyyy'})
+
+for row_num, row_data in enumerate(data):
+    for col_num, cell_value in enumerate(row_data):
+        if col_num == 22:
+            worksheet.write_datetime(row_num, col_num, cell_value, date_format)
+        else:
+            worksheet.write(row_num, col_num, cell_value)
+# Chiudi il workbook
+workbook.close()
---
+++ catalogo/keywords.py
@@ -0,0 +1,32 @@
+keywords = {
+    "Bradesco": {"severity": "high", "sector": "Finance", "tag": "Banking", "damage": ""},
+    "Itaú": {"severity": "high", "sector": "Finance", "tag": "Banking", "damage": ""},
+    "MasterCard": {"severity": "high", "sector": "Finance", "tag": "Banking", "damage": ""},
+    "Register.Br": {"severity": "high", "sector": "Finance", "tag": "Banking", "damage": ""},
+    "Nubank": {"severity": "high", "sector": "Finance", "tag": "Banking", "damage": ""},
+    "PIX": {"severity": "high", "sector": "Finance", "tag": "Banking", "damage": ""},
+    "Caixatem": {"severity": "high", "sector": "PA", "tag": "Banking", "damage": ""},
+    "Banco Do Brasil": {"severity": "high", "sector": "Finance", "tag": "Banking", "damage": ""},
+    "cielo": {"severity": "high", "sector": "Finance", "tag": "Banking", "damage": ""},
+
+
+
+    "vivo": {"severity": "medium", "sector": "Telco", "tag": "", "damage": ""},
+    "claro": {"severity": "medium", "sector": "Telco", "tag": "", "damage": ""},
+    "Tim": {"severity": "medium", "sector": "Telco", "tag": "", "damage": ""},
+
+
+    "Netflix": {"severity": "medium", "sector": "Entertainment", "tag": "Streaming Services", "damage": ""},
+    "Amazon Prime": {"severity": "medium", "sector": "Entertainment", "tag": "Streaming Services", "damage": ""},
+    "Spotify": {"severity": "medium", "sector": "Entertainment", "tag": "Streaming Services", "damage": ""},
+    "Amazon": {"severity": "medium", "sector": "Retail", "tag": "", "damage": ""},
+    "Microsoft": {"severity": "medium", "sector": "Software/Hardware", "tag": "", "damage": "furto dati"},
+
+
+
+    "La vittima riceve un messaggio che afferma che il tuo account e -mail dovrebbe passare attraverso un aggiornamento": {"severity": "medium", "sector": "Software/Hardware", "tag": "", "damage": "furto dati"},
+    "L'utente riceve un messaggio che informa che la capacità di archiviazione del proprio account e -mail ha raggiunto il limite ed è necessario eseguire un aggiornamento tramite il link disponibile, tuttavia, il messaggio è falso.": {"severity": "medium", "sector": "Software/Hardware", "tag": "", "damage": "furto dati"},
+    "La vittima riceve un'e -mail contenente un documento disponibile per il download, tuttavia il link disponibile è un URL dannoso.": {"severity": "medium", "sector": "Software/Hardware", "tag": "", "damage": "furto dati"},
+    "L'utente riceve un messaggio che afferma che esiste un ordine nel suo nome in attesa del pagamento, tuttavia, il messaggio è falso.": {"severity": "medium", "sector": "Critical Infrastructure", "tag": "", "damage": "denaro"},
+
+}
